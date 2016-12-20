78fdd2f205d56a28485b83ca6b83c26625526837
damadormoreno <amador.moreno.david@gmail.com>
2016-12-20 10:35:39 +0100

    Ejemplo pestañas(Tabs)

---
+++ Pestanias/app/src/main/java/david/moreno/amador/pestanias/MainActivity.java
@@ -0,0 +1,38 @@
+package david.moreno.amador.pestanias;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPagerAdapter viewPagerAdapter;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.ToolbarPrincipal);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.TabLayoutPrincipal);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+
+        viewPager = (ViewPager)findViewById(R.id.ViewPagerPrincipal);
+        //A nuestro adaptador le pasamos el manager y el método que nos cuenta los fragmentos del tablayout
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        //Asignamos al viewPager nuestro adaptador
+        viewPager.setAdapter(viewPagerAdapter);
+        //Asignamos al tablayout el viewpager
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ Pestanias/app/src/main/java/david/moreno/amador/pestanias/ViewPagerAdapter.java
@@ -0,0 +1,69 @@
+package david.moreno.amador.pestanias;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import david.moreno.amador.pestanias.fragments.Fragment1;
+import david.moreno.amador.pestanias.fragments.Fragment2;
+import david.moreno.amador.pestanias.fragments.Fragment3;
+
+/**
+ * Created by David on 19/12/2016.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    //Número de pestañas
+    private int numTabs;
+
+    public ViewPagerAdapter(FragmentManager fm, int numTabs) {
+        super(fm);
+        this.numTabs=numTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment;
+
+        switch (position){
+            case 0:
+                fragment= new Fragment1();
+                break;
+            case 1:
+                fragment = new Fragment2();
+                break;
+            case 2:
+                fragment = new Fragment3();
+                break;
+            default:
+                return null;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        //devolvemos el número de pestañas.
+        return numTabs;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String titulo=null;
+        switch (position){
+
+            case 0:
+                titulo="Pestaña 1";
+                break;
+            case 1:
+                titulo="Pestaña 2";
+                break;
+            case 2:
+                titulo = "Pestaña 3";
+                break;
+            default:
+                titulo = null;
+        }
+        return titulo;
+    }
+}
