4a90a436a01304ff6549130289800473b72f48e3
damadormoreno <amador.moreno.david@gmail.com>
2016-12-20 23:40:44 +0100

    Descripción ejemplo pestañas y añadidos iconos al menú y funcionalidad.

--- Pestanias/app/src/main/java/david/moreno/amador/pestanias/fragments/Fragment1.java
+++ Pestanias/app/src/main/java/david/moreno/amador/pestanias/fragments/Fragment1.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,7 +19,16 @@ public class Fragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+        View v =inflater.inflate(R.layout.fragment_fragment1, container, false);
+
+        setHasOptionsMenu(true);
+        return v;
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_principal, menu);
     }
 
 }
--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 Repositorio de ejemplos de Android.
 
 ##Ejemplo Pestañas (Pestanias)
+En este ejemplo usamos tres tabs, cada una apuntando a un fragmento que se ha diseñado con constraints. Cada una de las tabs posee un menu personalizado que responde a eventos de click.
+
+
 ![Tabs gif](img/tabs208x328.gif)
 
