8b3b99d1f6e2fae2545c57d602db21b90d3162e2
damadormoreno <amador.moreno.david@gmail.com>
2016-12-20 23:40:44 +0100

    Descripción ejemplo pestañas y añadidos iconos al menú y funcionalidad.

--- Pestanias/app/src/main/java/david/moreno/amador/pestanias/MainActivity.java
+++ Pestanias/app/src/main/java/david/moreno/amador/pestanias/MainActivity.java
@@ -2,24 +2,32 @@ package david.moreno.amador.pestanias;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener{
 
     ViewPagerAdapter viewPagerAdapter;
     ViewPager viewPager;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.ToolbarPrincipal);
+        toolbar = (Toolbar)findViewById(R.id.ToolbarPrincipal);
         setSupportActionBar(toolbar);
 
-        TabLayout tabLayout = (TabLayout)findViewById(R.id.TabLayoutPrincipal);
+        tabLayout = (TabLayout)findViewById(R.id.TabLayoutPrincipal);
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         tabLayout.setTabMode(TabLayout.MODE_FIXED);
 
@@ -40,8 +48,33 @@ public class MainActivity extends AppCompatActivity {
         TabLayout.Tab tabCall=tabLayout.getTabAt(0);
         TabLayout.Tab linux=tabLayout.getTabAt(1);
         TabLayout.Tab motorbike=tabLayout.getTabAt(2);
+
         tabCall.setIcon(R.drawable.selectorbox);
         linux.setIcon(R.drawable.selectorlinux);
         motorbike.setIcon(R.drawable.selectorbike);
+
+        //inflamos el menu al que queremos añadir eventos de click, posteriormente
+        //le decimos a qué actividad tiene que apuntar el click.
+        toolbar.inflateMenu(R.menu.menu_moto);
+        toolbar.setOnMenuItemClickListener(this);
+
+    }
+
+    //Implementamos Toolbar.OnMenuItemClickListener y su método, onMenu..., de este sacamos el
+    //item que han pulsado y hacemos lo que tengamos que hacer.
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_linux:
+                Toast.makeText(this, "Linux rules", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_moto:
+                Toast.makeText(this, "Precaución!", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return true;
+
     }
 }
--- Pestanias/app/src/main/java/david/moreno/amador/pestanias/fragments/Fragment2.java
+++ Pestanias/app/src/main/java/david/moreno/amador/pestanias/fragments/Fragment2.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,7 +18,15 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+        View v = inflater.inflate(R.layout.fragment_fragment2, container, false);
+        setHasOptionsMenu(true);
+
+        return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_moto, menu);
     }
 
 }
